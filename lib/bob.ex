bc59d58062353b9564b0e774fb81612461e88b4e
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-01-07 13:26:09 +0100

    Copy over Exercism Bob

--- lib/bob.ex
+++ lib/bob.ex
@@ -1,18 +1,20 @@
 defmodule Bob do
-  @moduledoc """
-  Documentation for `Bob`.
-  """
+  @spec hey(String.t()) :: String.t()
+  def hey(input) do
+    input = String.trim(input)
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Bob.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    cond do
+      silent?(input) -> "Fine. Be that way!"
+      shouting_question?(input) -> "Calm down, I know what I'm doing!"
+      question?(input) -> "Sure."
+      shouting?(input) -> "Whoa, chill out!"
+      true -> "Whatever."
+    end
   end
+
+  defp silent?(input), do: "" == input
+  defp shouting?(input), do: input == String.upcase(input) && letters?(input)
+  defp question?(input), do: String.ends_with?(input, "?")
+  defp shouting_question?(input), do: shouting?(input) && question?(input)
+  defp letters?(input), do: Regex.match?(~r/\p{L}+/, input)
 end
